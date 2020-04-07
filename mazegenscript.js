8db924e35d970ca88f11dbed9121b31504f4cd61
kuzgeo <kuzminykh53@yandex,ru>
2020-04-07 19:24:54 +0500

    Coloryzed + coin counter added

--- mazegenscript.js
+++ mazegenscript.js
@@ -1,14 +1,21 @@
 let screen = document.getElementById('screen');
 let ctx = screen.getContext('2d');
 let maze = [];
+cells = [];
 height = 19;
 width = 19;
-
+class Cell{
+    constructor(options){
+        this.x = options.x;
+        this.y = options.y;
+    }
+}
 for (let i = 0; i < height; i++){
     maze.push('')
     for (let j = 0; j < width; j++){
         if ((j % 2 == 0) || (j == width - 1) || (i == 0) || (i == height - 1) || (i % 2 == 0)){
             maze[i] += 'w';
+            cells.push
         } else {
             maze[i] += 'a';
         }
@@ -25,7 +32,5 @@ for (let i = 0; i < height; i++){
         }
         
     }
-
-
-
 }
+
--- scripts.js
+++ scripts.js
@@ -1,25 +1,28 @@
 let screen = document.getElementById("screen");
 let ctx = screen.getContext('2d');
+let scoreEl = document.getElementById("score");
 
+let score = 0;
 let width = 20;
 let height = 20; 
 
-let obj = [];
+let wall = [];
+let coin = [];
 
 let map = [
     'wwwwwwwwwwwwwwww',
-    'w              w',
-    'w              w',
-    'w              w',
-    'w              w',
-    'w              w',
-    'w              w',
-    'w              w',
-    'w   wwwww      w',
-    'w   w   w      w',
-    'w   w   wwwww  w',
+    'wsssssssssswwwww',
+    'wsssswwsssswsssw',
+    'wsssswwsssswsssw',
+    'wsssssssssswsssw',
+    'w    wwsssswsssw',
+    'w    wwsssswsssw',
+    'w    wwssssssssw',
+    'w   wwwwwssssssw',
+    'w   w   wssssssw',
+    'w   w   wwww   w',
     'w   w          w',
-    'w   w   wwwww  w',
+    'w   w   wwww   w',
     'w   w   w      w',
     'w   w   w      w',
     'wwwwwwwwwwwwwwww',
@@ -51,7 +54,7 @@ function createPlayer(x, y, rad, color, dx, dy, xprevious, yprevious){
     )
 }
 
-createPlayer(150, 150, 16, 'red', 0, 0, 0, 0);
+createPlayer(48, 48, 16, 'red', 0, 0, 0, 0);
 
 let walls = [];
 class Wall{
@@ -60,6 +63,15 @@ class Wall{
         this.y = options.y;
         this.width = options.width;
         this.height = options.height;
+        this.type = options.type;
+    }
+};
+
+class Coin{
+    constructor(options){
+        this.x = options.x;
+        this.y = options.y;
+        this.rad = options.rad;
         this.type = options.type;
     }
 };
@@ -77,6 +89,17 @@ function createWall(x, y, width, height, type){
     walls.push(newWall);
 }
 
+function createCoin(x, y, rad, type){
+    newCoin = new Coin(
+        {
+            x: x,
+            y: y,
+            rad: rad,
+            type: type,
+        }
+    );
+    coin.push(newCoin);
+}
 
 //create and draw map
 function createMap(){
@@ -87,7 +110,10 @@ function createMap(){
         for (let j = 0; j < map[i].length; j++){
             if (map[i][j] == "w"){
                 createWall(valOfSymbol, valOfString, 32, 32, 1);
-            } 
+            }  else if (map[i][j] == 's'){
+                createCoin(valOfSymbol, valOfString, 8, 2);
+                
+            }
             valOfSymbol+=32;
         };
 
@@ -96,11 +122,30 @@ function createMap(){
     }
 }
 
-function drawWall(){
+function drawWalls(){
     for (let i = 0; i < walls.length; i++){
-        ctx.fillRect(walls[i].x, walls[i].y, walls[i].width, walls[i].height);
+        if (walls[i].type == 1){
+            ctx.beginPath();
+            ctx.fillRect(walls[i].x, walls[i].y, walls[i].width, walls[i].height);
+            ctx.fillStyle = 'black';
+            ctx.fill();
+            ctx.closePath();
+        } 
     }
 }
+function drawCoin(){
+    for (let i = 0; i < coin.length; i++){
+        if(coin[i].type == 2){
+            ctx.beginPath();
+            ctx.arc(coin[i].x + 16,coin[i].y + 16,coin[i].rad,0,2*Math.PI);
+            ctx.fillStyle = 'yellow';
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+    
+    
 function drawPlayer(){
     ctx.beginPath();
     ctx.arc(player.x,player.y,16,0,2*Math.PI);
@@ -116,26 +161,37 @@ function loop(){
     
     //clear screen
     ctx.clearRect(0, 0, screen.width, screen.height);
-      
-    drawWall();
+    scoreEl.innerHTML = `Score: ${score}`;
+    drawWalls();
+    drawCoin();
     drawPlayer();
     player.xprevious = player.x;
     player.yprevious = player.y;
     player.x += player.dx;
     player.y += player.dy;
-//     if ((obj1.x + obj1.width >= obj2.x) && (obj1.x <= obj2.x + obj2.width)) XColl = true;
-//   if ((obj1.y + obj1.height >= obj2.y) && (obj1.y <= obj2.y + obj2.height)) YColl = true;
+
     for (let i = 0; i < walls.length; i++){
         if (
-        (walls[i].x < player.x + 12) &&
-        (walls[i].x + walls[i].width > player.x - 12)&&
-        (walls[i].y <  player.y + 12) &&
-        (walls[i].y + walls[i].height > player.y - 12))
+        (walls[i].x < player.x + 13) &&
+        (walls[i].x + walls[i].width > player.x - 13)&&
+        (walls[i].y <  player.y + 13) &&
+        (walls[i].y + walls[i].height > player.y - 13))
         {
             player.y = player.yprevious;
             player.x = player.xprevious;
         }}
-        
+    for (let i = 0; i < coin.length; i++){
+        if (
+            (coin[i].x < player.x + 13) &&
+            (coin[i].x + coin[i].rad > player.x - 13)&&
+            (coin[i].y <  player.y + 13) &&
+            (coin[i].y + coin[i].rad > player.y - 13))
+        {
+            coin.splice(i,1);
+            score += 1;
+        }
+    }
+
     
 }
 
@@ -144,32 +200,38 @@ window.addEventListener("keydown", function(event) {
 
     if (event.keyCode === 65) {
       player.dx = -4;
-      console.log('PRESS LEFT');
+    //   player.dy = 0;
+    //   console.log('PRESS LEFT: x: ' + player.x + ' y: ' + player.y);
     } else if(event.keyCode === 68){
         player.dx = 4;
-        console.log('PRESS RIGHT');
+        // player.dy = 0;
+        // console.log('PRESS RIGHT: x: ' + player.x + ' y: ' + player.y);
     } else if(event.keyCode === 87){
         player.dy = -4;
-        console.log('PRESS UP');
+        // player.dx = 0;
+        // console.log('PRESS UP: x: ' + player.x + ' y: ' + player.y);
     } else if(event.keyCode === 83){
         player.dy = 4;
-        console.log('PRESS DOWN');
+        // player.dx = 0;
+        // console.log('PRESS DOWN: x: ' + player.x + ' y: ' + player.y);
   }}
 )
 window.addEventListener("keyup", function(event) {
-
+    // player.dx = 0;
+    // player.dy = 0;
     if (event.keyCode === 65) {
       player.dx = 0;
-      console.log('UP LEFT');
+    //   console.log('UP LEFT');
     } else if(event.keyCode === 68){
         player.dx = 0;
-        console.log('UP RIGHT');
+        // console.log('UP RIGHT');
     } else if(event.keyCode === 87){
         player.dy = 0;
-        console.log('UP UP');
+        // console.log('UP UP');
     } else if(event.keyCode === 83){
         player.dy = 0;
-        console.log('UP DOWN');
-  }}
+        // console.log('UP DOWN');
+  }
+    }
 )
 setInterval(loop, 60);
